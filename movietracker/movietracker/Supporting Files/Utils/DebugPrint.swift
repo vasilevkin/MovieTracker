18ba36251653a95dbaeeb6df49d1faadbea9f3e7
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-24 17:52:24 +0300

    16 -  implement debug print log.

---
+++ movietracker/movietracker/Supporting Files/Utils/DebugPrint.swift
@@ -0,0 +1,15 @@
+//
+//  DebugPrint.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 24/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+func dLog(_ message: String, function: String = #function, line: Int = #line) {
+    #if DEBUG
+    print("\(function).\(line): \(message)")
+    #endif
+}
