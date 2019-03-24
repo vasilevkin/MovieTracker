9ecac7648b792528dc04a27bb3249619c747b24c
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-24 13:21:30 +0300

    4 - Add UI for main scene, add Featured view controller.

---
+++ movietracker/movietracker/Scenes/FeaturedViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FeaturedViewController.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 24/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class FeaturedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
