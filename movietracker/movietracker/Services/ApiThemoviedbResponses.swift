241581ec2493f02281c75cd459a629a07e46a549
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-25 21:17:37 +0300

    3 - Implement ApiThemoviedbResponses.

---
+++ movietracker/movietracker/Services/ApiThemoviedbResponses.swift
@@ -0,0 +1,25 @@
+//
+//  ApiThemoviedbResponses.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 25/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+struct AuthTokenResponse: Decodable {
+    let requestToken: String
+    
+    enum CodingKeys: String, CodingKey {
+        case requestToken = "request_token"
+    }
+}
+
+struct LoginResponse: Decodable {
+    let success: Bool
+}
+
+struct MoviesResponse: Decodable {
+    let results: [Movie]
+}
