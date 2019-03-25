47138d43bd4a052791245cd345e2b53a4acf3841
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-25 22:30:58 +0300

    3 - Implement Movie and Genre models.

---
+++ movietracker/movietracker/Models/Genre.swift
@@ -0,0 +1,14 @@
+//
+//  Genre.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 25/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+struct Genre: Decodable {
+    let genreId: Int
+    let name: String
+}
---
+++ movietracker/movietracker/Models/Movie.swift
@@ -0,0 +1,57 @@
+//
+//  Movie.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 25/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+struct Movie: Decodable {
+    let adult: Bool?
+    let backdropPath: String?
+    let budget: Int?
+    let genres: [Genre]?
+    let homepage: String?
+    let movieId: Int
+    let imdbId: String?
+    let originalLanguage: String?
+    let originalTitle: String?
+    let overview: String?
+    let popularity: Double?
+    let posterPath: String?
+    let releaseDate: String?
+    let revenue: Int?
+    let runtime: Int?
+    let status: String?
+    let tagline: String?
+    let title: String
+    let video: Bool?
+    let voteAverage: Double?
+    let voteCount: Int?
+    
+    enum CodingKeys: String, CodingKey {
+        case adult = "adult"
+        case backdropPath = "backdrop_path"
+        case budget = "budget"
+        case genres =  "genres"
+        case homepage = "homepage"
+        case movieId = "id"
+        case imdbId = "imdb_id"
+        case originalLanguage = "original_language"
+        case originalTitle = "original_title"
+        case overview = "overview"
+        case popularity = "popularity"
+        case posterPath = "poster_path"
+        case releaseDate = "release_date"
+        case revenue = "revenue"
+        case runtime = "runtime"
+        case status = "status"
+        case tagline = "tagline"
+        case title = "title"
+        case video = "video"
+        case voteAverage = "vote_average"
+        case voteCount = "vote_count"
+    }
+}
