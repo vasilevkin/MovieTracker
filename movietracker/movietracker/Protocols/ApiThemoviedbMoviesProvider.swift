5d9e252a03c603b182042057806198cb0237fed2
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-26 21:11:22 +0300

    3 - Add ApiThemoviedbMoviesProvider protocol.

---
+++ movietracker/movietracker/Protocols/ApiThemoviedbMoviesProvider.swift
@@ -0,0 +1,40 @@
+//
+//  ApiThemoviedbMoviesProvider.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 26/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#if !RX_NO_MODULE
+import RxSwift
+import RxCocoa
+#endif
+
+/**
+ Gets Movie data from themoviedb.org api
+ */
+protocol ApiThemoviedbMoviesProvider {
+    /**
+     Fetches popular movies from themoviedb.org api
+     
+     - Returns: Optional observable array of 'Movie' type
+     */
+    func fetchPopularMovies() -> Observable<[Movie]?>
+    
+    /**
+     Fetches movie details for single movieId from themoviedb.org api
+     
+     - Parameter movieId: unique movie identifier (id)
+     - Returns: Optional observable of 'Movie' type
+     */
+    func fetchMovieDetails(for movieId: Int) -> Observable<Movie?>
+    
+    /**
+     Fetches custom requested movies from themoviedb.org api
+     
+     - Parameter query: movie query string
+     - Returns: Optional observable array of 'Movie' type
+     */
+    func searchMovies(for query: String) -> Observable<[Movie]?>
+}
