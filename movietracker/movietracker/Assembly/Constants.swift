c10fce80fb9264bf523f1d959609d8f594c23999
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-27 21:10:12 +0300

    3 - Add a secret Constants struct.

---
+++ movietracker/movietracker/Assembly/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 27/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import Foundation
+
+/**
+ Contains all sensitive secret constants.
+ Should not be committed to repo with data in plain text.
+ Repo only contains a crypted version of this file.
+ 
+ This is just a data struct to show data format.
+ Actual data is crypted.
+ */
+private struct Constants {
+    static let themoviedbApiKey = "PUT_YOUR_KEY_HERE"
+}
