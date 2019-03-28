d21ba1724c7240e5e57b78bc56b4df56d0042503
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-28 22:20:22 +0300

    4 - Add MovieCollectionViewCell, reusable cell for main scene.

---
+++ movietracker/movietracker/Scenes/Reusable Views/MovieCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  MovieCollectionViewCell.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 28/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class MovieCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
