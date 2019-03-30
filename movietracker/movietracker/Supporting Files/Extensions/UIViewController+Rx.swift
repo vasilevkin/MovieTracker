c824144010f98a017e0f78d07b01b2a605a4a259
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-30 08:48:52 +0300

    4 - Add reactive extensions for UIViewController.

---
+++ movietracker/movietracker/Supporting Files/Extensions/UIViewController+Rx.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewController+Rx.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 30/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+#if !RX_NO_MODULE
+import RxSwift
+import RxCocoa
+#endif
+
+extension Reactive where Base: UIViewController {
+
+    var viewWillAppear: ControlEvent<Void> {
+        let source = self.methodInvoked(#selector(Base.viewWillAppear(_:))).map { _ in }
+        return ControlEvent(events: source)
+    }
+
+}
