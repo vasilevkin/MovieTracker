b4dfc89d6126616f97d437c754a13ec399c7f546
Sergey Vasilevkin <svasilev@mera.ru>
2019-03-31 17:32:35 +0300

    4 - Add coordinator for Featured scene with method stubs.

---
+++ movietracker/movietracker/Scenes/Featured/FeaturedCoordinator.swift
@@ -0,0 +1,27 @@
+//
+//  FeaturedCoordinator.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 31/03/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+final class FeaturedCoordinator: FeaturedCoordinatable {
+    
+    internal let navigationController: UINavigationController
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func navigateToMovieDetailScene(with movieId: Int, api: ApiThemoviedbService) {
+        
+    }
+    
+    func navigateToTVShowDetailScene(with tvShowId: Int, api: ApiThemoviedbService) {
+        
+    }
+    
+}
--- movietracker/movietracker/Scenes/Featured/FeaturedViewModel.swift
+++ movietracker/movietracker/Scenes/Featured/FeaturedViewModel.swift
@@ -26,6 +26,7 @@ final class FeaturedViewModel: ViewModelType {
     
     struct Dependencies {
         let api: ApiThemoviedb
+        let coordinator: FeaturedCoordinatable
     }
     
     private let dependencies: Dependencies
@@ -48,7 +49,7 @@ final class FeaturedViewModel: ViewModelType {
             }
             .share()
         
-//        let mappedResults = results
+        let mappedResults = results
             .map { movies, tvShows in
                 return [CarouselViewModel(movies: movies),
                         CarouselViewModel(tvShows: tvShows)]
@@ -60,6 +61,36 @@ final class FeaturedViewModel: ViewModelType {
             .selected
             .asObservable()
             .withLatestFrom(results) { ($0, $1 ) }
+            .do(onNext: { [weak self] (index: (Int, Int), result: ([Movie]?, [TVShow]?)) in
+                guard let strongSelf = self else {
+                    dLog("Unexpectedly found nil")
+                    return
+                }
+                let (carouselIndex, itemIndex) = index
+                let (movies, tvShows) = result
+                
+                switch carouselIndex {
+                case 0:
+                    guard let movieId = movies?[itemIndex].movieId else {
+                        dLog("Unexpectedly found nil")
+                        return
+                    }
+                    strongSelf.dependencies.coordinator.navigateToMovieDetailScene(
+                        with: movieId, api: strongSelf.dependencies.api)
+                    
+                case 1:
+                    guard let tvShowId = tvShows?[itemIndex].tvShowId else {
+                        dLog("Unexpectedly found nil")
+                        return
+                    }
+                    strongSelf.dependencies.coordinator.navigateToTVShowDetailScene(
+                        with: tvShowId, api: strongSelf.dependencies.api)
+
+                default:
+                    dLog("Unexpectedly found nil")
+                    return
+                }
+            })
             .map { _ in () }
             .asDriver(onErrorJustReturn: ())
         
