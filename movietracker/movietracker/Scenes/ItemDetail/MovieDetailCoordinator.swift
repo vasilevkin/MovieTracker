356f7ef4ee2945bf92b034fea457609a22cebf55
Sergey Vasilevkin <svasilev@mera.ru>
2019-04-10 21:55:08 +0300

    19 - Add MovieDetailCoordinator.

---
+++ movietracker/movietracker/Scenes/ItemDetail/MovieDetailCoordinator.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDetailCoordinator.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 06/04/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+final class MovieDetailCoordinator {
+
+}
