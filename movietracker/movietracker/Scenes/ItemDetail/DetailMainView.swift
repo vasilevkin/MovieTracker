be6be1a44d91512177aebb339aa22e1daeb2741d
Sergey Vasilevkin <svasilev@mera.ru>
2019-04-18 08:57:07 +0300

    25 - Add DetailMainView.

---
+++ movietracker/movietracker/Scenes/ItemDetail/DetailMainView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailMainView.swift
+//  movietracker
+//
+//  Created by Sergey Vasilevkin on 18/04/2019.
+//  Copyright © 2019 Sergey Vasilevkin. All rights reserved.
+//
+
+import UIKit
+
+class DetailMainView: UIView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
